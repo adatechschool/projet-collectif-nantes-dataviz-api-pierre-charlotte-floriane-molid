33e02c450aab36294688589d7f434e68806d26ff
flo <boireaufloriane@hotmail.fr>
2024-11-25 16:24:58 +0100

    fonction recupération validé

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-
 async function recupApi() { 
-    const reponse = await fetch("https://pokeapi.co/api/v2/pokemon/")
+    const reponse = await fetch("https://pokeapi.co/api/v2/pokemon")
     const recuperation = await reponse.json();
     console.log(recuperation)
-    console.log(recuperation.results)
-    
-    let affichageAPI = document.querySelector('h1')
-    for(let i=0; i< results.lenght; i++ ){
-    console.log(results[i])
-    affichageAPI.innerHTML += `<h1>${results[i].name}</h1>`
-
+    const affichage = document.querySelector('h1')
+    for(let i=0; i< recuperation.results.length; i++ ) {
+    affichage.innerHTML += `<h1>${recuperation.results[i].name}</h1>`
+    console.log(recuperation.results[i].name)
     }
 }
+
 recupApi()
