53a3084abb2b9627e580caea96bd5b50b1343656
Raika <pejuvenelle@hotmail.com>
2024-11-25 15:27:46 +0100

    Ajout de la fonction pour récuperer le nom des pokémon et affichage dans le html

---
+++ script.js
@@ -0,0 +1,12 @@
+async function recupApi(){
+    const reponse = await fetch('https://pokeapi.co/api/v2/pokemon')
+    const recuperation = await reponse.json();
+    const affichage = document.querySelector('h1')
+    for(let i=0; i< recuperation.results.length; i++){
+        affichage.innerHTML += `<h1>${recuperation.results[i].name}<h1>`
+        console.log(recuperation.results[i].name)
+    }
+
+}
+
+recupApi()
