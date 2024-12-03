fdd78b92d083d4b7b37139be0634665529c1d86b
Raika <pejuvenelle@hotmail.com>
2024-12-03 11:33:23 +0100

    Fonction suivant et précédent ajouter

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentPokemonList = [];
+
 async function recupApi() {
     const reponse = await fetch("https://tyradex.vercel.app/api/v1/types");
     const recuperation = await reponse.json();
@@ -14,6 +16,7 @@ async function recupApi() {
       type.addEventListener("click", async () => {
         const reponse2 = await fetch(`https://tyradex.vercel.app/api/v1/types/${recuperation[i].name.en}`)
         const recuperation2 = await reponse2.json() 
+        currentPokemonList = recuperation2.pokemons; // Met à jour la liste globale avec les Pokémon du type sélectionné
         console.log(recuperation2)
         const balise2 = document.getElementById('type-liste-container')
         balise2.innerHTML = "" // on vide
@@ -59,52 +62,63 @@ async function recupApi() {
 
   async function afficherDetailsPokemon(pokemonId) {
     try{
+        //Appel l'API et vide la balise 2 pour pouvoir afficher de nouveaux éléments.
         const reponse = await fetch(`https://tyradex.vercel.app/api/v1/pokemon/${pokemonId}`);
         const pokemon = await reponse.json();
         const balise2 = document.getElementById('type-liste-container');
         balise2.innerHTML = '';
-
+        
+        //Création d'une balise div avec un ID 'placement'
         const placement = document.createElement('div')
         placement.classList.add('placement')
 
+        //Création d'une balise h2 avec un ID 'nomPokemon' qui affiche le nom des pokémons en français.
         const detailsPokemon = document.createElement('h2')
         detailsPokemon.classList.add('nomPokemon')
         detailsPokemon.innerHTML = pokemon.name.fr
-        
-placement.appendChild(detailsPokemon);
+        //Ajout de la balise detailsPokemon(h2) en tant d'enfant de la balise placement(div).
+        placement.appendChild(detailsPokemon);
 
+        //Création d'un container pour récupérer les balises images créer ci-dessous pour afficher les images des pokémons.
         const imagesContainer = document.createElement('div');
         imagesContainer.style.display = 'flex'; // Affiche les images en ligne
         imagesContainer.style.gap = '20px'; // Espace entre les images
         placement.appendChild(imagesContainer)
 
+        //Création d'un container (Gmax) pour récupérer les balises imagesGmax créer ci-dessous pour afficher les images des pokémons.
         const imagesContainerGmax = document.createElement('div');
         imagesContainerGmax.style.display = 'flex'; // Affiche les images en ligne
         imagesContainerGmax.style.gap = '20px'; // Espace entre les images
         placement.appendChild(imagesContainerGmax)
 
+        //Création d'une balise pour afficher les images normal des pokémons.
         const imgRegular = document.createElement('img')
         imgRegular.classList.add('sprites')
         imgRegular.src = pokemon.sprites.regular
         imagesContainer.appendChild(imgRegular)
         
+        //Création d'une balise pour afficher les images shiny des pokémons.
         const imgShiny = document.createElement('img')
         imgShiny.classList.add('sprites')
         imgShiny.src = pokemon.sprites.shiny
         imagesContainer.appendChild(imgShiny)
 
+        //Création d'une balise pour afficher les images normal Gmax des pokémons.
         if(pokemon.sprites.gmax){
             const imgGmax= document.createElement('img')
             imgGmax.classList.add('sprites')
             imgGmax.src = pokemon.sprites.gmax.regular
             imagesContainerGmax.appendChild(imgGmax)
         }
+
+        //Création d'une balise pour afficher les images shiny Gmax des pokémons.
         if(pokemon.sprites.gmax){
             const imgGmax= document.createElement('img')
             imgGmax.classList.add('sprites')
             imgGmax.src = pokemon.sprites.gmax.shiny
             imagesContainerGmax.appendChild(imgGmax)
         }
+
         // Créer un conteneur flex pour les types
         const typesContainer = document.createElement('div');
         typesContainer.style.display = 'flex';
@@ -133,11 +147,51 @@ placement.appendChild(detailsPokemon);
             talent.innerText = talentInfo.name;
             talentsContainer.appendChild(talent);
         });
+
+        const navContainer = document.createElement('div');
+        navContainer.style.position = 'relative';
+        navContainer.style.display = 'flex';
+        navContainer.style.justifyContent = 'space-between'; // Place les boutons aux extrémités
+        navContainer.style.marginBottom = '10px'; // Espace sous les boutons
+
+        const Suivant = document.createElement('button')
+        Suivant.classList.add('Next-Button')
+        Suivant.innerText ="Suivant"
+        navContainer.appendChild(Suivant)
+        Suivant.addEventListener('click', () =>{
+          //permet de vérifier si dans l'id du pokedex_id est égale a celui de pokemponId
+          const currentIndex = currentPokemonList.findIndex(p => p.pokedex_id === pokemonId);
+          //Vérifie que le pokémon existe dans la liste et que ce n'est pas le dernier élément du tableau.
+          if (currentIndex !== -1 && currentIndex < currentPokemonList.length - 1) {
+              const nextPokemonId = currentPokemonList[currentIndex + 1].pokedex_id;
+              afficherDetailsPokemon(nextPokemonId); // Passe au Pokémon suivant
+          } else {
+              alert("Il n'y a pas de Pokémon suivant.");
+           }
+        });
+
+        const Precedent = document.createElement('button')
+        Precedent.classList.add('Prev-Button')
+        Precedent.innerText ="Précédent"
+        navContainer.appendChild(Precedent)
+        Precedent.addEventListener('click', () =>{
+          const currentIndex = currentPokemonList.findIndex(p => p.pokedex_id === pokemonId);
+          //Si l'index est supérieur a 0 on retourne au pokémon précedent, si jamais ce n'est pas le cas il y a une alerte.
+          if (currentIndex > 0) {
+              const nextPokemonId = currentPokemonList[currentIndex - 1].pokedex_id;
+              afficherDetailsPokemon(nextPokemonId); // Passe au Pokémon précédent
+          } else {
+              alert("Il n'y a pas de Pokémon précédent.");
+           }
+        });
+        
+        
         placement.appendChild(talentsContainer);
+        //regroupe les différentes balises en tant qu'enfant de la balise 2
+        balise2.appendChild(navContainer)
         balise2.appendChild(placement)
         }catch (erreur) {
         console.error('Erreur lors de la récupération des détails du Pokémon:', erreur);
         }
-    
-}
+  }
   recupApi()
